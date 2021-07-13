ab2dc0b3c25a89f7fe37d5b7bcc9bbf8d6a3634e
rh <rh@rhs-MacBook-Pro.local>
2021-07-13 15:44:51 +0900

    only spec

--- setup.py
+++ setup.py
@@ -4,33 +4,33 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="fits_reader",
-    version="0.1.0",
-    author="RH",
-    author_email="kuma@kwansei.ac.jp",
-    description="fits reade module'",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/RH-REP/CIBER2/fits_reader",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License ::  Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    # entry_points = {
-    #     'console_scripts': ['sample_command = sample_command.sample_command:main']
-    # },
-    python_requires='>=3.7',
-)
+# setuptools.setup(
+#     name="fits_reader",
+#     version="0.1.0",
+#     author="RH",
+#     author_email="kuma@kwansei.ac.jp",
+#     description="fits read module'",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/RH-REP/CIBER2/fits_reader",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License ::  Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     # entry_points = {
+#     #     'console_scripts': ['sample_command = sample_command.sample_command:main']
+#     # },
+#     python_requires='>=3.7',
+# )
 
 setuptools.setup(
     name="spec_reader",
     version="0.1.0",
     author="RH",
     author_email="kuma@kwansei.ac.jp",
-    description="fits reade module'",
+    description="spec read module'",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RH-REP/CIBER2/spec_reader",
